b55c56ef313678c5850c9c8cf54e68946f0d46f8
Harshini <hchellasamy@hawk.iit.edu>
2020-04-21 15:07:16 -0500

    add javascript to js file

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function myMap() {
+  var mapProp = {
+    center:new google.maps.LatLng(41,-87),
+    zoom:4,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
