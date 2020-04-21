3aeb83fd4f66999a1f1301b98cc5108d70a3eed7
Harshini <hchellasamy@hawk.iit.edu>
2020-04-21 16:25:42 -0500

    add 3 extra features to map

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(41,-87),
-      zoom:4,
-    };
-    
-   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-   }
+  var chicago = new google.maps.LatLng(41.850, -87.650);
+  
+  var mapOptions = {
+    zoom: 4,
+    center: chicago,
+    mapTypeId: 'hybrid'
+  };
+  
+  var googleMap = new google.maps.Map(document.getElementById('googleMap'),
+    mapOptions);
+  
+  var marker = new google.maps.Marker({
+    map: googleMap,
+    position: chicago,
+    animation: google.maps.Animation.DROP,
+    title: 'Chicago Marker'
+  });
+
+}
