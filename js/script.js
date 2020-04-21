0a9f3dfed697561c18ff925339bc8a0d2ac714d8
Harshini <hchellasamy@hawk.iit.edu>
2020-04-21 16:03:02 -0500

    add box border on index.html and fix map

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function myMap() {
-  var mapProp = {
-    center:new google.maps.LatLng(41,-87),
-    zoom:4,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  }
+    var mapProp= {
+      center:new google.maps.LatLng(41,-87),
+      zoom:4,
+    };
+    
+   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+   }
