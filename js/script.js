22f50393cac92f1c07f440bc8151462f66847cbe
Harshini <hchellasamy@hawk.iit.edu>
2020-04-28 15:12:25 -0500

    fix gallery

--- js/script.js
+++ js/script.js
@@ -46,6 +46,6 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[0].style.display = "block";
+  dots[0].className += " active";
 } 
